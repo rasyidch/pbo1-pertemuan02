7f61d0865fcc4fb793fa36081b7d6c2c08a71016
rasyidch <rasyid.chamiedy@gmail.com>
2020-03-30 00:09:49 +0800

    Memanggil method dari banyak Class Package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== data mahasiswa =====");
+  System.out.println("NPM : 18631005");
+  System.out.println("Nama : Ibadur Rasyid Chamiedy");
+  System.out.println("================================");
+ }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== data mata kuliah =====");
+  System.out.println("Kode : TIF3402");
+  System.out.println("Nama : Program berbasis objek");
+  System.out.println("================================");
+ }
+}
